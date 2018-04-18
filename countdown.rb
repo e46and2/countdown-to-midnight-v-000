f06c021c35b8cc0547d2a7537cd805cb8d011d03
Emily Nicholl Liepe <e.nicholl.liepe@gmail.com>
2018-04-18 20:26:33 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -3,7 +3,7 @@ x = 10
 
 def countdown(x)
   while x > 0 
-    puts "#{x} SECOND(S)!".sleep(1)
+    puts "#{x} SECOND(S)!"
     x -= 1
   end
   x = "HAPPY NEW YEAR!"
@@ -11,7 +11,7 @@ end
 
 def countdown_with_sleep(x)
   while x > 0 
-    puts "#{x} SECOND(S)!"
+    puts "#{x} SECOND(S)!".sleep(1)
     x -= 1
   end
   x = "HAPPY NEW YEAR!"
